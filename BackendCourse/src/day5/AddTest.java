b4ce62a4952c57f12ac5bcb7ea90a3950389d4a1
ramos-vini <ramoscontrole@gmail.com>
2023-05-02 18:40:18 +0200

    day5

---
+++ BackendCourse/src/day5/AddTest.java
@@ -0,0 +1,19 @@
+package day5;
+
+public class AddTest {
+
+	public static void main(String[] args) {
+		// function that adds a & b and prints the result
+		
+		int a = 5;
+		int b = 7;
+		
+		myAdd(a, b);
+		
+	}
+	
+	public static void myAdd(int a, int b) {
+		 System.out.println(a+b);
+		}
+
+}
---
+++ BackendCourse/src/day5/Main.java
@@ -0,0 +1,9 @@
+package day5;
+
+public class Main {
+	// Functions and Modules
+	
+	public static void main(String[] args) {
+		
+	}
+}
