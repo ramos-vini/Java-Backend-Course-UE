007bfcad06fb4dfd8ab672abc57da71443ebef67
ramos-vini <ramoscontrole@gmail.com>
2023-05-09 16:08:52 +0200

    Ex3

---
+++ BackendCourse/src/day3/Ex3.java
@@ -0,0 +1,29 @@
+package day3;
+import java.util.Scanner;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Choose a whole number between 1-100 to be compared with a random number.");
+		int n = scanner.nextInt();
+		
+		int random = (int)Math.round(Math.random()*100);
+		
+		System.out.println("Chosen number: "+n);
+		System.out.println("Random number: "+random+"\n");
+		
+		if(n > random) {
+			System.out.println("Your number is greater than the Random one.");
+		} else if(n == random) {
+			System.out.println("Your number equals the Random one :O");
+		} else {
+			System.out.println("Your number is smaller than the Random one.");
+		}
+		
+		scanner.close();
+	}
+
+}
