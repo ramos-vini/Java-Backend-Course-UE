c7c4c15bf72800af920c30ae4265c1ca7245be35
ramos-vini <ramoscontrole@gmail.com>
2023-05-16 13:42:08 +0200

    Ex8

---
+++ BackendCourse/src/day3/Ex8.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class Ex8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Printing the odd integer numbers from 50-0:\n");
+		
+		for(int i = 49; i >= 0; i-=2) {
+			System.out.println(i);
+		}
+	}
+
+}
