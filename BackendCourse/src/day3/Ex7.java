850dc77884b5dfaebe75ff5ebffd3c49fabea6a7
ramos-vini <ramoscontrole@gmail.com>
2023-05-16 13:39:46 +0200

    Ex7

---
+++ BackendCourse/src/day3/Ex7.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class Ex7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Printing even integer numbers from 0-50:\n");
+		
+		for (int i = 0; i <= 50; i+=2) {
+			System.out.println(i);
+		}
+	}
+
+}
