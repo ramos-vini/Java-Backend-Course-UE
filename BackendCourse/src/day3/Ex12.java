536519c750d0463ea128db2386ea592de5607b88
ramos-vini <ramoscontrole@gmail.com>
2023-05-17 19:17:19 +0200

    Ex12

--- BackendCourse/src/day3/Ex12.java
+++ BackendCourse/src/day3/Ex12.java
@@ -7,20 +7,31 @@ public class Ex12 {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Gamblers Program! You win 50% more of what you have bet or you lose it.\nType in your stake ($):");
+		System.out.println("Gambling Game! You win 50% more of what you have bet or you lose it all.\nType in your stake ($):");
 		int stake = scanner.nextInt();
-		int goal = (int) (stake*1.5);
-		System.out.println("Your goal is to receive $ "+goal+".00. Let's go!");
+		int goal = (int)(stake*1.5);
+		System.out.println("Your goal is to receive $ " + goal + ".00. Let's go!");
 		
-		int ammount = stake;
+		int sum = stake;
 		
-		while(ammount > 0 && ammount < goal) {
-			//int aux = Math.round(Math.random());
-			
-			System.out.print(ammount+" -> ");
-			//ammount += aux;
-			System.out.println(ammount+" ");
+		while(sum > 0 && sum < goal) {
+			int aux = (int)Math.round(Math.random());
+			if(aux == 0) {
+				aux = -1;
+			}
+			System.out.print(sum + (aux==1 ? " (+1$) " : " (-1$) "));
+			sum += aux;
+			System.out.println("-> " + sum);
 		}
+		
+		System.out.println("\nFINAL REWARD: $ "+sum+".00\n");
+		
+		if(sum != 0) {
+			System.out.println("Yess!! You did it, time to go to Vegas!! :)");
+		} else {
+			System.out.println("Yeah, sometimes gambling is not the best idea...");
+		}
+		
 		
 		scanner.close();
 	}
