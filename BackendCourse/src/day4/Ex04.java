cc925d2a894c6d44e23a8f27d9a8dc6c66208fcd
ramos-vini <ramoscontrole@gmail.com>
2023-05-19 15:54:42 +0200

    Ex04

--- BackendCourse/src/day4/Ex04.java
+++ BackendCourse/src/day4/Ex04.java
@@ -1,30 +1,30 @@
 package day4;
 import java.util.Random;
 
-public class Ex03 {
+public class Ex04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 3;
-		int[][] a = new int[n][n]; 
-		int[][] b = new int[n][n];
-		int[][] c = new int[n][n];
-		
+
+		int[][] a = new int[4][3];
+		int[][] b = new int[3][2];
+		int[][] c = new int[2][3];
+
 		Random random = new Random();
-		
+
 		System.out.println("Array A:");
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
 				a[i][j] = random.nextInt(10);
 				System.out.print(a[i][j] + " ");
 			}
 			System.out.println();
 		}
 		System.out.println("-------");
-		
+
 		System.out.println("Array B:");
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
+		for (int i = 0; i < b.length; i++) {
+			for (int j = 0; j < b[i].length; j++) {
 				b[i][j] = random.nextInt(10);
 				System.out.print(b[i][j] + " ");
 			}
@@ -33,15 +33,15 @@ public class Ex03 {
 		System.out.println("-------");
 		
 		System.out.println("Array C:");
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < c[i].length; j++) {
 				c[i][j] = a[i][j] * b[j][i];
 				System.out.print(c[i][j] + " ");
 			}
 			System.out.println();
 		}
 		System.out.println("-------");
-		
+
 	}
 
 }
