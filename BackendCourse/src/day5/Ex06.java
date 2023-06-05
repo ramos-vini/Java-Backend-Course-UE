ba9e4abb5a684081e56c6ec0b5e38e71b5662259
ramos-vini <ramoscontrole@gmail.com>
2023-06-05 14:36:00 +0200

    TODO: Ex06 (Hanoi Tower)

--- BackendCourse/src/day5/Ex06.java
+++ BackendCourse/src/day5/Ex06.java
@@ -1,10 +1,33 @@
 package day5;
 
+import java.util.*;
+
 public class Ex06 {
 
 	public static void main(String[] args) {
-		// Tower of Hanoi using Recursion
+		// TODO: Tower of Hanoi using Recursion
 		
+		ArrayList<String> discs = new ArrayList<String>(Arrays.asList
+				(
+						"---------------",
+						" ------------- ",
+						"  -----------  ",
+						"   ---------   ",
+						"    -------    ",
+						"     -----     ",
+						"      ---      ",
+						"       -       "
+				)
+				);
+		
+		ArrayList<ArrayList<String>> pegs = new ArrayList<ArrayList<String>>();
+		pegs.add(discs);
+		
+		for(ArrayList<String> peg : pegs) {
+			
+			System.out.println(peg);
+			System.out.println("###############\n");
+		}
 	}
 
 }
