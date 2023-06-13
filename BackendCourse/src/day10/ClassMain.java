53ef290449888319a0c3a336822bfe41611c3ce0
ramos-vini <ramoscontrole@gmail.com>
2023-06-13 18:41:28 +0200

    day10

---
+++ BackendCourse/src/day10/ClassMain.java
@@ -0,0 +1,13 @@
+package day10;
+
+public class ClassMain {
+
+	public static void main(String[] args) {
+		// Arrow Functions
+		// Data Types and Generic type
+		// Hashing - Set, HashTable
+		// 
+		
+	}
+
+}
